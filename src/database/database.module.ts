fe03e433edb49371697a43208005bc700a7255b7
Ayush Saxena <ayushsaxena38@gmail.com>
2025-12-12 13:07:50 +0530

    added remote mysql uri in sequelize module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,17 +1,28 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
     imports:[
+        ConfigModule.forRoot({
+         isGlobal: true,
+        }),
         SequelizeModule.forRoot({
             dialect:'mysql',
-            host:'localhost',
-            port:3306,
-            username:'root',
-            password:'1234',
-            database:'bayut_clone_db',
+            // host:'localhost',
+            // port:3306,
+            // username:'root',
+            // password:'1234',
+            // database:'bayut_clone_db',
+            uri:process.env.MYSQLURI,//"mysql://root:KOIHuhwgwHrcCMKcLsxfIkCTlIWWLjWc@crossover.proxy.rlwy.net:48064/railway",
             autoLoadModels:true,
-            synchronize:true
+            synchronize:true,
+            logging:false,
+            pool:{
+                max:5,
+                min:0,
+                idle:1000
+            }
         })
     ]
 })
